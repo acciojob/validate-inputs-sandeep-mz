2cce85b114521dfe6d8330408ce376b23f48ea22
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-10 20:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+ function validateName(input) {
+	const namePattern = /[A-Za-z]{3,}/;
+	if (namePattern.test(input.value)) {
+		input.className = "valid";
+	} else {
+		input.className = "invalid";
+	}
+}
+
+function validateEmail(input) {
+	const emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+	if (emailPattern.test(input.value)) {
+		input.className = "valid";
+	} else {
+		input.className = "invalid";
+	}
+}
+
+function validateMobile(input) {
+	const mobilePattern = /[0-9]{10}/;
+	if (mobilePattern.test(input.value)) {
+		input.className = "valid";
+	} else {
+		input.className = "invalid";
+	}
+}
